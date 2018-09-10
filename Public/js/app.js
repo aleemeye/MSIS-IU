25e84ddba9c4bd16d1f95f9500105bd9cae55a27
Alex Meyer <aleemeye@iu.edu>
2018-09-10 09:56:10 -0400

    Update

--- Public/js/app.js
+++ Public/js/app.js
@@ -2,79 +2,80 @@ var dashboardApp = new Vue({
   el: '#chart',
 
   data:{
-  project: {
-  'name': '',
-  'short_description': '',
-  'start_date': '',
-  'target_date': '',
-  'budget': '',
-  'spent': '',
-  'projected_spend': '',
-  'weekly_effort_target': '',
-},
-tasks:[
+    project: {
+      'name': '',
+      'short_description': '',
+      'start_date': '',
+      'target_date': '',
+      'budget': '',
+      'spent': '',
+      'projected_spend': '',
+      'weekly_effort_target': '',
+    },
+    tasks:[
 
-  {
-  'id': '',
-  'title': '',
-  'type': '',
-  'size': '',
-  'team': '',
-  'status': '',
-  'start_date': '',
-  'close_date': '',
-  'hours_worked': '',
-  'perc_complete': '',
-  'current_sprint': ''
-  }
-]
-},
+      {
+        'id': '',
+        'title': '',
+        'type': '',
+        'size': '',
+        'team': '',
+        'status': '',
+        'start_date': '',
+        'close_date': '',
+        'hours_worked': '',
+        'perc_complete': '',
+        'current_sprint': ''
+      }
+    ]
+  },
 
   computed:{
     days_left: function() {
-      return moment(this.project.target_date).diff(moment(), 'days')}
-  },
+      return this.project.target_date ?
+        '0' : moment(this.project.target_date).diff(moment(), 'days')}
+    },
     pretty_target_date: function(){
       return this.pretty_date(this.project.target_date)
     },
 
-  methods:{
-    pretty_date: function(d){
-      return moment(d).format('l')
-    },
-    pretty_currency: function(val){
-      if (val < 1e3){
-        return '$' + val
+    methods:{
+      pretty_date: function(d){
+        return moment(d).format('l')
+      },
+      pretty_currency: function(val){
+        if (val < 1e3){
+          return '$' + val
+        }
+        if (val < 1e6){
+          return '$' + (val/1e3).toFixed(1) + 'k'
+        }
+        return '$' + (val/1e6).toFixed(1) + 'M'
+      },
+      fetchTasks() {
+        fetch('https://raw.githubusercontent.com/tag/iu-msis/dev/public/p1-tasks.json')
+        .then( response => response.json())
+        //is the same as .then(function (response){return response.json()})
+        .then(json => {this.tasks = json})
+        .catch(function(err){
+          console.log('FETCH ERROR');
+          console.log(err);
+        })
+      },
+      fetchData() {
+        fetch('https://raw.githubusercontent.com/tag/iu-msis/dev/public/project1.json')
+        .then( response => response.json())
+        //is the same as .then(function (response){return response.json()})
+        .then(json => {this.project = json})
+        .catch(function(err){
+          console.log('FETCH ERROR');
+          console.log(err);
+        })
       }
-      if (val < 1e6){
-        return '$' + (val/1e3).toFixed(1) + 'k'
-      }
-      return '$' + (val/1e6).toFixed(1) + 'M'
     },
-    fetchTasks() {
-      fetch('https://raw.githubusercontent.com/tag/iu-msis/dev/public/p1-tasks.json')
-      .then( response => response.json())
-      //is the same as .then(function (response){return response.json()})
-      .then(json => {this.tasks = json})
-      .catch(function(err){
-        console.log('FETCH ERROR');
-        console.log(err);
-      })
-    },
-    fetchData() {
-      fetch('https://raw.githubusercontent.com/tag/iu-msis/dev/public/project1.json')
-      .then( response => response.json())
-      //is the same as .then(function (response){return response.json()})
-      .then(json => {this.project = json})
-      .catch(function(err){
-        console.log('FETCH ERROR');
-        console.log(err);
-      })
-    }
-  },
-  created: function() {
-    this.fetchTasks();
-    this.fetchData();
+    created: function() {
+      this.fetchTasks();
+      this.fetchData();
 
-  }
-})
+    }
+  })
