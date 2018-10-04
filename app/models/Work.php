f63d368c7ef32f72207c09d6f935928328b6a19a
Alex Meyer <aleemeye@iu.edu>
2018-10-04 16:10:04 -0400

    idek

--- app/models/Work.php
+++ app/models/Work.php
@@ -27,8 +27,8 @@ class Work
 
   public function create() {
     $db = new PDO(DB_SERVER, DB_USER, DB_PW);
-    $sql = 'INSERT Work (task_id, team_id, start_date, hours, completion_estimate)
-            VALUES (?, ?, ?, ?, ?)';
+    $sql = 'INSERT INTO Work (id, task_id, team_id, start_date, hours, completion_estimate)
+            VALUES (?, ?, ?, ?, ?, ?)';
 
     $statement = $db->prepare($sql);
     $success = $statement->execute([
